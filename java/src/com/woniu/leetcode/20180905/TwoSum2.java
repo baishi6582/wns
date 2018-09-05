74e45d50d9d39ea5c436fb938ea2b0f1456728dc
baishi6582 <401251096@qq.com>
2018-09-05 23:58:04 +0800

    两数之和 II - 输入有序数组

---
+++ java/src/com/woniu/leetcode/20180905/TwoSum2.java
@@ -0,0 +1,71 @@
+package leetcode.date_20180905;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ 	题目描述：两数之和 II - 输入有序数组
+ 	
+	给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+
+	函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+	
+	说明:
+	
+	返回的下标值（index1 和 index2）不是从零开始的。
+	你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+	示例:
+	
+	输入: numbers = [2, 7, 11, 15], target = 9
+	输出: [1,2]
+	解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+	
+	解题思路：由于已经有序，所以两边遍历，寻找即可解决问题，总得来说比较简单
+	
+	题目链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/description/
+ * @author z00364813o
+ *
+ */
+public class TwoSum2 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+    	int start = 0;
+    	int end = nums.length -1;
+    	while(start < end) {
+    		if(nums[start] + nums[end] == target) {
+    			result[0] = start + 1;
+    			result[1] = end + 1;
+    			break;
+    		}
+    		
+    		while(nums[start] + nums[end] > target && start < end) {
+    			end --;
+    		}
+    		while(nums[start] + nums[end] < target && start < end) {
+    			start ++;
+    		}
+    	}
+    	return result;
+    }
+    
+    public int[] twoSum2(int[] nums, int target) {
+        int[] result = new int[2];
+    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    	for(int i=0; i<nums.length; i++) {
+    		if(!map.containsKey(target - nums[i])) {
+    			map.put(nums[i], i);
+    		} else {
+    			result[0] = map.get(target - nums[i]) + 1;
+    			result[1] = i + 1;
+    		}
+    	}
+    	return result;
+    }
+    
+    public static void main(String[] args) {
+		int[] nums = {2,3,3};//{3,2,4};
+		int[] result = new TwoSum2().twoSum(nums, 6);
+		System.out.println(result[0]  + " " + result[1]);
+	}
+}
